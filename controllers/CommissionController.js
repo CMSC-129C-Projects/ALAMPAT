2a1e02fc16671f9fd84cce3363060941ca73e27c
carlthebarnacle@gmail.com <81558680+legendavid09@users.noreply.github.com>
2021-05-16 17:39:55 +0800

    Updated Commission add and retrieve

---
+++ controllers/CommissionController.js
@@ -0,0 +1,182 @@
+const User = require('../models/user')
+const ObjectId = require("mongodb").ObjectID
+const bcrypt = require('bcryptjs')
+const Commission = require('../models/commissions')
+
+const addCommission = async(req, res, next) => {
+    try{
+
+            let commission = new Commission ({
+                _id: new ObjectId(),
+                commissionname: req.body.commissionname, 
+                images: {
+                    filename: req.body.commissionimage.filename, //hashedfile,
+                    contentType: req.body.commissionimage.contentType,
+                    imageBase64: req.body.commissionimage.imageBase64
+                },
+                description:req.body.commissiondescription,
+                slot:req.body.slot,
+                price:req.body.price
+            })
+            if (commission.length <= 0){
+                return res.send('You must select atleast 1 file.')
+            }
+
+            commission.save(function(err, result){
+                if(!err){
+                
+                User.findByIdAndUpdate( req.params.id , { $push: { Commissions: result._id } })
+                    .then((result) => {
+                        //console.log(result)
+                        res.json({
+                            message: "Commission Id added to User's Commissions and saved succesfully! ",
+                            success: true,
+                        })
+                        
+                    }).catch((error)=>{
+                        res.status(400).json({
+                            message: "Commission Id adding failed to User's commissions!",
+                            error: error,
+                            success: false,
+                        })
+                        
+                    })
+    
+                }else{
+                    res.json({
+                        message: 'Commission Save Failed! Image file name already existed!',
+                        success: false,
+                        err
+                    })
+                }
+            })
+
+        //})
+        
+    }
+    catch(error){
+    
+        console.log(error)
+        res.status(404).json({ 
+            error,
+            message: "duh",
+            success: false, })
+    
+    }
+    //updates the user object data to the database 
+    
+}
+
+const getCommissionList = async(req, res, next) => {
+    try{
+        const user = await User.findById(req.params.id)
+            .populate( 'commissions')
+            
+        if(user){
+        //console.log(results);
+        res.status(200).json({
+            commissionsArray: user.commissions
+        })
+
+        }else{
+            res.status(400).json({
+                message: "Can't get product data",
+                error: err,
+                success: false,
+            })
+        }
+            
+    } catch(error){
+        console.log(error)
+        res.status(404).json({ 
+            error,
+            success: false, })
+    }
+}
+
+const updateCommission = async(req, res, next) => {
+    try {
+        
+            //creates a new user object together with the final image object
+            let commission = new Commissions ({
+                productname: req.body.productname, 
+                images: {
+                    filename: hashedfile,
+                    contentType: req.body.productimage.contentType,
+                    imageBase64: req.body.productimage.imageBase64
+                },
+                description:req.body.productdescription,
+            })
+        //updates the user object data to the database 
+            Commissions.findByIdAndUpdate( req.params.productid , commission)
+                .then((result) => {
+                    //console.log(result)
+                    res.json({
+                        message: 'Commission data updated successfully!',
+                        result,
+                        success: true,
+                    })
+                    
+                }).catch((error)=>{
+                    res.status(400).json({
+                        message: 'Commission data update failed!',
+                        error:error,
+                        user,
+                        success: false,
+                    })
+                
+                })
+        
+        
+    }
+    catch (error) {
+        console.log(error)
+        res.status(400).json({ 
+            message: 'Commission update process failed',
+            error,
+            success: false, })
+    }
+}
+
+
+const deleteCommission = async(req, res, next) => {
+    try {
+        
+            //creates a new user object together with the final image object
+           
+        //updates the user object data to the database 
+        Commissions.findByIdAndRemove(req.body._id, function(err, result){
+            if(!err){
+                User.findByIdAndUpdate( req.params.id , { $pull: { commissions: result._id } })
+                .then((data)=>{
+                    res.json({
+                        message: 'Commission data removed successfully!',
+                        data: data,
+                        success: true,
+                    })
+                }).catch((error)=>{
+                    res.status(400).json({
+                        message: 'Commission data removing failed!',
+                        error:error,
+                        user,
+                        success: false,
+                    })
+                })
+            }
+        })
+
+        
+    }
+    catch (error) {
+        console.log(error)
+        res.status(400).json({ 
+            message: 'Commission  data removing process failed',
+            error,
+            success: false, })
+    }
+}
+
+
+module.exports = { 
+    addCommission, getCommissionList, updateCommission, deleteCommission
+}
--- routes/seller.js
+++ routes/seller.js
@@ -4,6 +4,7 @@ const checkAuth = require('../middleware/checkAuth');
 
 const portfolioController = require('../controllers/PortfolioController');
 const productController = require('../controllers/productController');
+const commissionController = require('../controllers/CommissionController');
 
 router.post('/:id/addportfolio', portfolioController.addArtwork)
 router.get('/:id/portfolio', portfolioController.getArtworkList)
@@ -14,5 +15,10 @@ router.post('/:id/addproduct', productController.addProduct)
 router.get('/:id/products', productController.getProductList)
 router.patch('/:id/editproduct/:productid', productController.updateProduct)
 router.delete('/:id/removeproduct/:productid', productController.deleteProduct)
+
+router.post('/:id/addcommission', commissionController.addCommission)
+router.get('/:id/commissions', commissionController.getCommissionList)
+router.patch('/:id/editcommission/:commissionid', commissionController.updateCommission)
+router.delete('/:id/removecommission/:commissionid', commissionController.deleteCommission)
 
 module.exports = router;
