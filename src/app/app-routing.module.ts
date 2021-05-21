294978a3d3c84b4232c836bb4828d10b07678a1f
Littoo <80081180+Littoo@users.noreply.github.com>
2021-05-21 10:03:41 +0800

    Updated Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,14 +10,18 @@ import { ProductsComponent } from './components/accounts/seller/products/product
 import { LoginComponent } from './components/login/login.component';
 import { AddProductComponent } from './components/accounts/seller/add-product/add-product.component';
 import { AuthGuard } from './auth.guard';
-
+import { SellershopComponent } from './components/sellershop/sellershop.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: 'welcome', component: WelcomeComponent },
   { path: 'my-accounts-seller', component: MyaccountsellerComponent, canActivate: [AuthGuard] },
   { path: 'my-accounts-buyer', component: MyaccountbuyerComponent, canActivate: [AuthGuard] },
-  { path: 'seller-products', component: ProductsComponent },
+  { path: 'seller-shop', component: SellershopComponent, canActivate: [AuthGuard]  },
+  { path: 'seller-portfolio', component: PortfolioComponent, canActivate: [AuthGuard]  },
+  //{ path: 'seller-commission', component: ProductsComponent, canActivate: [AuthGuard]  },
+  { path: 'seller-products', component: ProductsComponent, canActivate: [AuthGuard]  },
   { path: 'notfound', component: NoPageFoundComponent},
   { path: '**', redirectTo:'notfound' }
 ];
--- src/app/components/sellershop/sellershop.component.ts
+++ src/app/components/sellershop/sellershop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-sellershop',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellershopComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  GotoPortfolio(){
+    this.router.navigate(['/seller-portfolio'])
+  }
+
+  GotoProducts(){
+    this.router.navigate(['/seller-products'])
+  }
+
+  GoToServices(){
+
+  }
 }
