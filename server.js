41f832647260a376e485ebb51a4ba2ba90f64d0e
Littoo <80081180+Littoo@users.noreply.github.com>
2021-05-29 17:02:57 +0800

    Updated package.json

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./src/app'));
+app.use(express.static('./dist/ALAMPAT'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('app-component.html', {root: 'src/app'}),
+    res.sendFile('index.html', {root: 'dist/ALAMPAT/'}),
 );
 
 // Start the app by listening on the default Heroku port
