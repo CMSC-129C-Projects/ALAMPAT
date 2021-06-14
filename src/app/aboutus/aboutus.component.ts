24a6cffc7dc81b58d63a6efb1c7146b741557442
Josiah Ocampo <cmocampo4@up.edu.ph>
2021-06-14 16:01:59 +0800

    Updated Header
    Updated typical Users' header into specific AboutUs header with navigation

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-aboutus',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutusComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  GoToMarketplace(){
+    this.router.navigate(['/marketplace'])
+}
 
 }
