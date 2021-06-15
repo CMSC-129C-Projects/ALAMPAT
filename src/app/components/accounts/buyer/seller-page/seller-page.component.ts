2b9f6ab4830d2627038a002e023cba4a8b4759c5
lavenderscent <80081344+lavenderscent@users.noreply.github.com>
2021-06-15 16:32:59 +0800

    UI of seller page

--- src/app/components/accounts/buyer/seller-page/seller-page.component.ts
+++ src/app/components/accounts/buyer/seller-page/seller-page.component.ts
@@ -11,14 +11,35 @@ export class SellerPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    //For Tabs
+    const tabs = document.querySelectorAll('.tabs li');
+    const tabContentBoxes = document.querySelectorAll('#tab-content > div');
+
+    tabs.forEach((tab: any) => {
+      tab.addEventListener('click', () => {
+        tabs.forEach(item => item.classList.remove('is-active'))
+        tab.classList.add('is-active');
+
+        const target = tab.dataset.target;
+        tabContentBoxes.forEach( box => {
+          console.log(target);
+          if (box.getAttribute('id') == target) {
+            box.classList.remove('is-hidden');
+          } else {
+            box.classList.add('is-hidden');
+          }
+        });
+      })
+    })
   }
 
   customOptions: OwlOptions = {
     loop: true,
     mouseDrag: true,
-    touchDrag: false,
-    pullDrag: false,
-    dots: false,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
     navSpeed: 700,
     navText: ['', ''],
     responsive: {
@@ -26,15 +47,15 @@ export class SellerPageComponent implements OnInit {
         items: 1
       },
       400: {
-        items: 2
+        items: 1
       },
       740: {
-        items: 3
+        items: 1
       },
       940: {
-        items: 4
+        items: 1
       }
     },
-    nav: true
+    nav: false
   }
 }
