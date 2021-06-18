3b86a0c462ed7a0debbbd38cdf9481932480f044
lavenderscent <80081344+lavenderscent@users.noreply.github.com>
2021-06-18 17:54:55 +0800

    Added payment options in checkout page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -67,5 +67,6 @@ export const routingComponents = [
   AddProductComponent,
   ReservationsComponent,
   ReservationSellerComponent,
+  CheckoutComponent,
   AboutusComponent
 ]
--- src/app/components/accounts/buyer/checkout/checkout.component.ts
+++ src/app/components/accounts/buyer/checkout/checkout.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { OrderService } from 'src/app/services/order';
-import { Subscription} from 'rxjs';
-
+import { Subscription, BehaviorSubject} from 'rxjs';
+import {AngularFireStorage} from '@angular/fire/storage';
 interface details {
   _id: string;
   service: {
@@ -35,17 +35,21 @@ interface details {
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit, OnDestroy {
+  reserv_id: string = '';
 
-  reserv_id: string = ''
+  checkout_sub: Subscription;
+  checkout_details: details;
 
-  checkout_sub: Subscription
-  
-  checkout_details: details
+  payment_option: any;
+  imageSRC : any = '';
+  fileName: string = '';
+  halfprice: any;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private orderserv: OrderService,
+    private afStorage: AngularFireStorage
   ) {
     this.setDetails()
   }
@@ -96,6 +100,7 @@ export class CheckoutComponent implements OnInit, OnDestroy {
   getCheckoutdetails(){
     this.checkout_sub = this.orderserv.getCheckoutdetails(this.reserv_id).subscribe( item => {
       this.checkout_details = item.data.details
+      this.halfprice = item.data.details.service.price/2
       console.log( JSON.stringify(item.data))
     })
   }
@@ -103,5 +108,45 @@ export class CheckoutComponent implements OnInit, OnDestroy {
     this.router.navigate(['/reservation/'])
   }
 
+  selectChangeHandler (event: any) {
+    //update the ui
+    this.payment_option = event.target.value;
+  }
+
+  uploadFile(event: Event) {
+    const target = event.target as HTMLInputElement
+
+    const file: File = (target.files as FileList)[0]
+    //Storage Path
+    const path =  `/Service/${Date.now()}_` + file.name
+    
+    //reference to storage bucket
+    const ref = this.afStorage.ref(path)
+
+    //main task 
+    //this.task = this.afStorage.upload(path, file)
+
+    //upload progress monitoring
+    //this.percentage = this.task.percentageChanges() ;
+    
+
+    //this.snapshot = this.task.snapshotChanges().pipe(
+    //  finalize( async() => {
+    //    this.url = await ref.getDownloadURL().toPromise()
+    //    this.serviceForm.patchValue({
+    //      commissionimage: {
+    //        filename: file.name,
+    //        contentType: file.type,
+    //        imageBase64: this.url
+    //      }
+    //    });
+        this.fileName = file.name
+    //    this.prev_image = this.imageSRC
+    //    this.imageSRC = this.url
+    //    console.log("Here: " + JSON.stringify(this.url) );
+    //  })
+    //)
+    //this.editimage.nativeElement.value = null
+  } 
 
 }
